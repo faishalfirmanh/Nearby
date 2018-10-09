d8bc3e80cfb1ffe34ce123dff189a89e7be9c452
isal48 <isalredarmy1@gmail.com>
2018-10-09 10:15:08 +0700

    coba

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -34,30 +34,30 @@ export class MapPage {
     console.log('ionViewDidLoad MapPage');
   }
                 initMap() {
-                navigator.geolocation.getCurrentPosition((location) => {
-                  console.log(location);
-              	  Map= new google.maps.Map(this.mapElement.nativeElement, {
-                    center: {lat: location.coords.latitude, lng: location.coords.longitude},
-                    zoom: 15
-                  });
-
-                  infowindow = new google.maps.InfoWindow();
-                  var service = new google.maps.places.PlacesService(map);
-                  service.nearbySearch({
-                    location: {lat: location.coords.latitude, lng: location.coords.longitude},
-                    radius: 1000,
-                    type: ['store']
-                  }, (results,status) => {
-                    if (status === google.maps.places.PlacesServiceStatus.OK) {
-                      for (var i = 0; i < results.length; i++) {
-                        this.createMarker(results[i]);
-                      }
-                    }
-                  });
-                }, (error) => {
-                  console.log(error);
-                }, Option);
-                var myplace = {lat: -33.8665, lng: 151.1956};
+                // navigator.geolocation.getCurrentPosition((location) => {
+                //   console.log(location);
+              	//   Map= new google.maps.Map(this.mapElement.nativeElement, {
+                //     center: {lat: location.coords.latitude, lng: location.coords.longitude},
+                //     zoom: 15
+                //   });
+                //
+                //   infowindow = new google.maps.InfoWindow();
+                //   var service = new google.maps.places.PlacesService(map);
+                //   service.nearbySearch({
+                //     location: {lat: location.coords.latitude, lng: location.coords.longitude},
+                //     radius: 1000,
+                //     type: ['store']
+                //   }, (results,status) => {
+                //     if (status === google.maps.places.PlacesServiceStatus.OK) {
+                //       for (var i = 0; i < results.length; i++) {
+                //         this.createMarker(results[i]);
+                //       }
+                //     }
+                //   });
+                // }, (error) => {
+                //   console.log(error);
+                // }, Option);
+                // var myplace = {lat: -33.8665, lng: 151.1956};
               }
 
         createMarker(place) {
